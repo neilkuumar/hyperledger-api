0b8878995a9d8b95b2224c6adaa5dd41c835d705
Neil Kumar <neilkumar@Neils-MacBook-Pro.local>
2018-07-13 18:01:46 +0100

    clean

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
+/**
+ * Express setup
+ */
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
